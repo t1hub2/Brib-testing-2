8a7666f4119abea392f1d084e80e860ed4861a16
t1hub2 <solavolodimir@gmail.com>
2024-12-09 20:42:37 +0100

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "T1 hub / Первый русский скрипт",
-   Icon = 129826331604126, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = codesandbox, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Загрузка",
    LoadingSubtitle = "T1",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
