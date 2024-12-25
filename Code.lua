373f847215ebaab4a4167560bc006249ecba19af
t1hub2 <solavolodimir@gmail.com>
2024-12-25 14:07:49 +0100

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -441,6 +441,7 @@ local Button = Tab:CreateButton({
 local Tab = Window:CreateTab("Создатели и тд.",4483362458)
 local Paragraph = Tab:CreateParagraph({Title = "Создатели", Content = "T1 и SCRIPTINGER"})
 local Paragraph = Tab:CreateParagraph({Title = "Телеграм", Content = "https://t.me/T1hubb"})
+local Paragraph = Tab:CreateParagraph({Title = "Наш сайт (часто не работает)", Content = "https://49eff2e2-7363-4a4d-b446-da965aba3be7-00-2ffaxarc4up04.sisko.replit.dev/home/"})
 
 local sound = Instance.new("Sound", game.Workspace)
 sound.SoundId = "rbxassetid://8503531171"
