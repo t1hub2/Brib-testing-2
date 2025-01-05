c9a40732a25e06d5636796bfe94dc35b446f1335
t1hub2 <solavolodimir@gmail.com>
2025-01-05 11:37:36 +0100

    Update Perechodnanovijintrfc.lua

--- Perechodnanovijintrfc.lua
+++ Perechodnanovijintrfc.lua
@@ -115,3 +115,10 @@ local Button = Tab:CreateButton({
          loadstring(game:HttpGet("https://raw.githubusercontent.com/CatEnddroid/Seek-Plushie/refs/heads/main/SeekPlushie.lua"))()-- The function that takes place when the button is pressed
     	end
 })
+local Button = Tab:CreateButton({
+	Name = "Путеводный сканер",
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+         loadstring(game:HttpGet("https://pastebin.com/raw/iBBqfYzn"))()-- The function that takes place when the button is pressed
+    	end
+})
