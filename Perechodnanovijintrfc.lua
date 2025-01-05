6895f11bf4a09a4aa72ac23f76a38287d0ca5d3a
t1hub2 <solavolodimir@gmail.com>
2025-01-05 21:58:01 +0100

    Update Perechodnanovijintrfc.lua

--- Perechodnanovijintrfc.lua
+++ Perechodnanovijintrfc.lua
@@ -310,3 +310,10 @@ local Button = Tab:CreateButton({
          loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Doors-Mode-Remakes/refs/heads/main/HardcoreFixed.lua"))()-- The function that takes place when the button is pressed
     	end
 })
+local Button = Tab:CreateButton({
+	Name = "ржавый отель",
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+         loadstring(game:HttpGet("https://gist.githubusercontent.com/T0mix1608/ff0a607a510996f361c230a2de85f9e8/raw/7096f3796a8feb208b6c0b9029686150e878b7a1/gistfile1.txt", true))()-- The function that takes place when the button is pressed
+    	end
+})
