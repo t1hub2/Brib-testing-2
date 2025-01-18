b0b30a8cb521bd1a536f59aab8ac8a22268cc00c
t1hub2 <solavolodimir@gmail.com>
2025-01-18 11:55:46 +0100

    Update 😈.lua

--- 😈.lua
+++ 😈.lua
@@ -1,6 +1,6 @@
 local Link = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/UI-Libraries/refs/heads/main/Playstation%20Ui%20Library/source.lua"))()
 
-local Win = Link:CreateWindow('T1 hub [REMAKE]','От Т1 и SCRIPTINGER') -- :CreateWindow(Title,Info) Note: info no space
+local Win = Link:CreateWindow('T1 hub [REMAKE] [BETA]','От Т1 и SCRIPTINGER') -- :CreateWindow(Title,Info) Note: info no space
 
 local Tab = Win:addPage('Предметы', 2, true, 6) -- :addPage(Title, Scoll Size, Visible, elementspacing)
 -- Note: Dont have update function
