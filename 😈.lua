a8765fa1b6ef0ed906c3236c7dbd14844cdb67f5
Mimukauwa <solavolodimir@gmail.com>
2025-02-01 10:48:50 +0100

    Релиз

--- 😈.lua
+++ 😈.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/t1hub2/Brib-testing-2/refs/heads/main/Notification"))()
 local Link = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/UI-Libraries/refs/heads/main/Playstation%20Ui%20Library/source.lua"))()
 
-local Win = Link:CreateWindow('T1 hub [REMAKE] [BETA]','От Т1 и SCRIPTINGER') -- :CreateWindow(Title,Info) Note: info no space
+local Win = Link:CreateWindow('T1 hub: Remake','От Т1 и SCRIPTINGER') -- :CreateWindow(Title,Info) Note: info no space
 
 local Tab = Win:addPage('Предметы', 2, true, 6) -- :addPage(Title, Scoll Size, Visible, elementspacing)
 -- Note: Dont have update function
