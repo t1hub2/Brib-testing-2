838995cc9362aa579d4522e36aa2b738201a8aba
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 09:07:43 +0100

    Update Tsb.lua

--- Tsb.lua
+++ Tsb.lua
@@ -34,21 +34,3 @@ local Window = Rayfield:CreateWindow({
 })
 
 local Tab = Window:CreateTab("Анимации спавна", 4483362458) -- Tab title and image
-local Button = Tab:CreateButton({
-   Name = "Сильнейший Герой",
-   Callback = function()
-      local player = game.Players.LocalPlayer
-      -- Wait until the character exists and has a Humanoid
-      repeat wait() until player.Character and player.Character:FindFirstChild("Humanoid")
-      
-      local character = player.Character or player.CharacterAdded:Wait()
-      local humanoid = character:FindFirstChild("Humanoid")
-      local UserInputService = game:GetService("UserInputService") -- (Only needed if you plan to use it later)
-
-      local anim2 = Instance.new("Animation")
-      anim2.AnimationId = "rbxassetid://15957366251"
-
-      local playAnim2 = humanoid:LoadAnimation(anim2)
-      playAnim2:Play()
-   end,
-})
