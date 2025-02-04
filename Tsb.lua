4bfdbf1df733f960248e861bb74226f9872c4271
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 08:43:21 +0100

    Update Tsb.lua

--- Tsb.lua
+++ Tsb.lua
@@ -1,52 +1,54 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "T1 hub: Remake [TSB]",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 for no icon.
    LoadingTitle = "Загрузка...",
    LoadingSubtitle = "От Скрипттингера и Т1",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   DisableBuildWarnings = false, -- Prevents warnings when there's a version mismatch
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil, -- Optional custom folder name
       FileName = "Big Hub"
    },
 
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false, -- Prompt the user to join your Discord server if supported
+      Invite = "noinvitelink", -- Just the invite code (without discord.gg/)
+      RememberJoins = true
    },
 
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = false, -- Set to true to enable the key system
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- Use a unique filename to avoid conflicts
+      SaveKey = true, -- Saves the user's key locally
+      GrabKeyFromSite = false, -- Change to true if pulling the key from a website
+      Key = {"Hello"} -- List of accepted keys
    }
 })
-local Tab = Window:CreateTab("Анимации спавна", 4483362458) -- Title, Image
+
+local Tab = Window:CreateTab("Анимации спавна", 4483362458) -- Tab title and image
 local Button = Tab:CreateButton({
    Name = "Сильнейший Герой",
    Callback = function()
-    local player = game.Players.LocalPlayer
-repeat wait() until player.Character.Humanoid
-local humanoid = player.Character.Humanoid
-local character = player.Character or player.CharacterAdded:Wait()
-local UserInputService = game:GetService("UserInputService")
+      local player = game.Players.LocalPlayer
+      -- Wait until the character exists and has a Humanoid
+      repeat wait() until player.Character and player.Character:FindFirstChild("Humanoid")
+      
+      local character = player.Character or player.CharacterAdded:Wait()
+      local humanoid = character:FindFirstChild("Humanoid")
+      local UserInputService = game:GetService("UserInputService") -- (Only needed if you plan to use it later)
 
-local anim2 = Instance.new("Animation")
-anim2.AnimationId = "rbxassetid://15957366251"
+      local anim2 = Instance.new("Animation")
+      anim2.AnimationId = "rbxassetid://15957366251"
 
-local playAnim2 = humanoid:LoadAnimation(anim2)
-playAnim2:Play()
-
+      local playAnim2 = humanoid:LoadAnimation(anim2)
+      playAnim2:Play()
    end,
 })
