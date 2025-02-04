c2e66fb827a51cdd19564f9c9f319f0cfa134cf0
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 07:52:03 +0100

    Update Tsb.lua

--- Tsb.lua
+++ Tsb.lua
@@ -1,6 +1,6 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-   Name = "T1 hub: Remake",
+   Name = "T1 hub: Remake [TSB]",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Загрузка...",
    LoadingSubtitle = "От Скрипттингера и Т1",
