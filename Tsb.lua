bbecfca365054ea4372b8841701c29df23aa303a
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 08:00:53 +0100

    Update Tsb.lua

--- Tsb.lua
+++ Tsb.lua
@@ -1,49 +1,56 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local success, Rayfield = pcall(function()
+    return loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+end)
+
+if not success then
+    warn("Failed to load Rayfield library")
+    return
+end
+
 local Window = Rayfield:CreateWindow({
-   Name = "T1 hub: Remake [TSB]",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Загрузка...",
-   LoadingSubtitle = "От Скрипттингера и Т1",
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+    Name = "T1 hub: Remake [TSB]",
+    Icon = "", -- Use a valid Roblox Image ID or Lucide Icon string.
+    LoadingTitle = "Загрузка...",
+    LoadingSubtitle = "От Скрипттингера и Т1",
+    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = nil, -- Create a custom folder for your hub/game
+        FileName = "Big Hub"
+    },
 
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
+    Discord = {
+        Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+        Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
 
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+        Title = "Untitled",
+        Subtitle = "Key System",
+        Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+        Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
 })
+
 local Tab = Window:CreateTab("Анимации спавна", 4483362458) -- Title, Image
 local Button = Tab:CreateButton({
-   Name = "Сильнейший Герой",
-   Callback = function()
-    local player = game.Players.LocalPlayer
-repeat wait() until player.Character.Humanoid
-local humanoid = player.Character.Humanoid
-local character = player.Character or player.CharacterAdded:Wait()
-local UserInputService = game:GetService("UserInputService")
-local anim2 = Instance.new("Animation")
-anim2.AnimationId = "rbxassetid://15957366251"
-local playAnim2 = humanoid:LoadAnimation(anim2)
-playAnim2:Play()
-   end,
+    Name = "Сильнейший Герой",
+    Callback = function()
+        local player = game.Players.LocalPlayer
+        repeat wait() until player.Character and player.Character:FindFirstChildOfClass("Humanoid")
+        local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+        local anim2 = Instance.new("Animation")
+        anim2.AnimationId = "rbxassetid://15957366251"
+        local playAnim2 = humanoid:LoadAnimation(anim2)
+        playAnim2:Play()
+    end,
 })
