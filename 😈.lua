0163ff8ff5ed63ac4e04e3caaf70fea08c8e087e
Mimukauwa <solavolodimir@gmail.com>
2025-02-05 07:01:16 +0100

    Update 😈.lua

--- 😈.lua
+++ 😈.lua
@@ -90,8 +90,7 @@ local Toggle = Tab:CreateToggle({
    CurrentValue = false,
    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-
-   game.Players.LocalPlayer.Character:SetAttribute("CanJump", true)
+    game.Players.LocalPlayer.Character:SetAttribute("CanJump", true)
    game.Players.LocalPlayer.Character:SetAttribute("CanJump", false)
    end,
 })
