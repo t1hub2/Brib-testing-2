139693b76f670a552e99cbff493ce820ccac208b
Mimukauwa <solavolodimir@gmail.com>
2025-04-09 19:49:34 +0200

    Update Newuibruhcuzbroken.lua

--- Newuibruhcuzbroken.lua
+++ Newuibruhcuzbroken.lua
@@ -155,6 +155,32 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/LolSeek/SallyStuff/re
     end,
 })
 
+local Button = Tab:Button({
+    Title = "Шейдеры от меня",
+    Desc = "OH MY PCCCEEEE",
+    Callback = function()
+        print("Clicked!")
+local Bloom = Instance.new("BloomEffect", game.Lighting)
+Bloom.Intensity = 0.12
+Bloom.Size = 9e9
+Bloom.Threshold = 0.05
+local DepthOfField = Instance.new("DepthOfFieldEffect", game.Lighting)
+DepthOfField.FarIntensity = 0.3
+DepthOfField.FocusDistance = 20
+DepthOfField.InFocusRadius = 0
+DepthOfField.NearIntensity = 0
+local SunRays = Instance.new("SunRaysEffect", game.Lighting)
+SunRays.Intensity = 0.1
+SunRays.Spread = 0.8
+local ColorCorrection = Instance.new("ColorCorrectionEffect", game.Lighting)
+ColorCorrection.Brightness = 0.025
+ColorCorrection.Contrast = 0.13
+ColorCorrection.Saturation = 0.15
+game.Lighting.GlobalShadows = false
+game.Lighting.OutdoorAmbient = Color3.fromRGB(35, 35, 45)
+    end,
+})
+
 local Tab = Window:Tab({
     Title = "Отель минус",
     Icon = "folder", -- lucide or rbxassetid
