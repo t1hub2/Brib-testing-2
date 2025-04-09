44d7a882165aab4c68052bf95f42beba620cbd84
Mimukauwa <solavolodimir@gmail.com>
2025-04-09 18:55:30 +0200

    Update Newuibruhcuzbroken.lua

--- Newuibruhcuzbroken.lua
+++ Newuibruhcuzbroken.lua
@@ -31,3 +31,31 @@ Window:EditOpenButton({
     Enabled = true,   -- true or false
     Draggable = true, -- true or false
 })
+
+local Tab = Window:Tab({
+    Title = "Предметы",
+    Icon = "folder", -- lucide or rbxassetid
+})
+
+local Button = Tab:Button({
+    Title = "Ножнецы",
+    Desc = "Уничтожитель 3000",
+    Callback = function()
+        print("Clicked!")
+            -- Spawner Customizations --
+_G.Luck = 100 -- The chance of shears spawning in the game
+_G.Spawns = false -- If the shears can spawn naturally in the game
+-- Tool Customizations --
+_G.Durability = 5 -- How much durability it will have until it breaks
+_G.MaxDurability = 5 -- The max durability if you enable regeneration
+_G.InfiniteUses = false -- If The Shears have infinite uses and cannot break
+_G.DurabilityTakenEachSnip = 0.25 -- How much each snip you take with the shears takes from the durability
+_G.SnipSpeed = 1.25 -- The speed of the snipping
+-- Regeneration --
+_G.Regens = true -- If the shears regenerate after being used
+_G.RegenSpeed = 1 -- How fast the shears will regenerate after being used
+_G.RegenAmount = 0.1 -- How much durability it will regenerate after the chosen amount of time
+-- Loadstring --
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iimateiYT/Scripts/main/Buffed%20Shears%20On%20Everything.lua"))()
+    end,
+})
