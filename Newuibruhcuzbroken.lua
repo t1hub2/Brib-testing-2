454217184c84d298682be6e978a661b7ca38b225
Mimukauwa <solavolodimir@gmail.com>
2025-04-09 16:47:33 +0200

    Update Newuibruhcuzbroken.lua

--- Newuibruhcuzbroken.lua
+++ Newuibruhcuzbroken.lua
@@ -1,1 +1,33 @@
+local WindUI = loadstring(game:HttpGet("https://tree-hub.vercel.app/api/UI/WindUI"))()
 
+local Window = WindUI:CreateWindow({
+    Title = "T1 hub", -- UI Title
+    Icon = "image", -- Url or rbxassetid or lucide
+    Author = ".ftgs", -- Author & Creator
+    Folder = "T1hubRemake3", -- Folder name for saving data (And key)
+    Size = UDim2.fromOffset(580, 460), -- UI Size
+    KeySystem = { -- Creates key system
+        Key = "T1hub_FREE_NOT_RANDOM_3343335", -- key
+        Note = "Получи ключ через Get key", -- Note
+        URL = "https://lootdest.org/s?PXJLZF1X", -- URL To get key (example: Discord)
+        SaveKey = true, -- Saves the key in the folder specified above
+    }, 
+    Transparent = true,-- UI Transparency
+    Theme = "Dark", -- UI Theme
+    SideBarWidth = 170, -- UI Sidebar Width (number)
+    HasOutline = true, -- Adds Outlines to the window
+})
+
+Window:EditOpenButton({
+    Title = "Open Example UI", -- Title
+    Icon = "monitor", -- Icon
+    CornerRadius = UDim.new(0,10), -- Radius
+    StrokeThickness = 2, -- Stroke Thickness
+    Color = ColorSequence.new( -- Gradient
+        Color3.fromHex("FF0F7B"), 
+        Color3.fromHex("F89B29")
+    ),
+    Position = UDim2.new(0.5,0,0.5,0), -- Position
+    Enabled = true,   -- true or false
+    Draggable = true, -- true or false
+})
