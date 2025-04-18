b377cc04ef1eb8c42bd6126279a7eea6039dd03b
Mimukauwa <solavolodimir@gmail.com>
2025-04-18 11:39:19 +0200

    Update Testlol.lua

--- Testlol.lua
+++ Testlol.lua
@@ -1,4 +1,4 @@
-local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
+local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
